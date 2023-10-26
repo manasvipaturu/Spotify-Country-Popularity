6530f40deb0a20349ecf4a1ef53f79fe7c3384f5
manasvipaturu <90577048+manasvipaturu@users.noreply.github.com>
2023-10-25 21:02:32 -0500

    Created music_analysis.py

---
+++ music_analysis.py
@@ -0,0 +1,23 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the 'universal_top_spotify_songs.csv' dataset
+data = pd.read_csv('universal_top_spotify_songs.csv')
+
+# Group the data by 'country'
+grouped_data = data.groupby('country')
+
+# Analyze and visualize the popularity of top songs by country
+plt.figure(figsize=(12, 6))
+#uses Kernel Density Estimation (KDE) plots to visualize the popularity distribution of top songs in different countries.
+for country, group in grouped_data:
+    sns.kdeplot(group['popularity'], label=country, shade=True)
+
+#plot 
+plt.xlabel('Popularity')
+plt.ylabel('Density')
+plt.title('Popularity Distribution of Top Songs in Different Countries')
+plt.legend()
+plt.show()
